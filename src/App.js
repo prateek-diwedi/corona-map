ee4afb4baedfe60031f15ad22c7e1859470aec3d
Prateek Diwedi <diwedi1112@gmail.com>
2020-04-09 22:18:07 +0530

    full app now available

--- src/App.js
+++ src/App.js
@@ -79,15 +79,15 @@ function App() {
         paint: {
           "circle-opacity": 0.75,
           "circle-stroke-width": [
-                 "interpolate",
-                 ["linear"],
-                 ["get", "cases"],
-                 1, 1,
-                 500, 1.20,
-                 10000, 1.35,
-                 50000, 1.50,
-                 100000, 1.75,
-               ],
+            "interpolate",
+            ["linear"],
+            ["get", "cases"],
+            1, 1,
+            500, 1.20,
+            10000, 1.35,
+            50000, 1.50,
+            100000, 1.75,
+          ],
           "circle-radius": [
             "interpolate",
             ["linear"],
@@ -110,6 +110,73 @@ function App() {
           ],
         }
       });
+
+      // create mapbox popup
+      const popup = new mapboxgl.Popup({
+        closeButton: false,
+        closeOnClick: false
+      });
+
+      // Variable to hold the active country/province on hover
+      let lastId;
+
+      //Mouse hover event
+      map.on("mouseover", "circles", e => {
+        //get id from properties
+        const id = e.features[0].properties.id;
+
+        // Only if the id are different we process the tooltip
+        if (id !== lastId) {
+          lastId = id;
+
+          // Change the pointer type on move move
+          map.getCanvas().style.cursor = "pointer";
+
+          const { cases, deaths, country, province } = e.features[0].properties;
+          const coordinates = e.features[0].geometry.coordinates.slice();
+
+          // Get all data for the tooltip
+          const countryISO =
+            lookup.byCountry(country)?.iso2 || lookup.byInternet(country)?.iso2;
+
+          const provinceHTML =
+            province !== "null" ? `<p>Province: <b>${province}</b></p>` : "";
+
+          const mortalityRate = ((deaths / cases) * 100).toFixed(2);
+
+          const countryFlagHTML = Boolean(countryISO)
+            ? `<img src="https://www.countryflags.io/${countryISO}/flat/64.png"></img>`
+            : "";
+
+          const HTML = `<p>Country: <b>${country}</b></p>
+              ${provinceHTML}
+              <p>Cases: <b>${cases}</b></p>
+              <p>Deaths: <b>${deaths}</b></p>
+              <p>Mortality Rate: <b>${mortalityRate}%</b></p>
+              ${countryFlagHTML}`;
+
+          // Ensure that if the map is zoomed out such that multiple
+          // copies of the feature are visible, the popup appears
+          // over the copy being pointed to.
+          while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+          }
+
+          popup
+            .setLngLat(coordinates)
+            .setHTML(HTML)
+            .addTo(map);
+        }
+      });
+
+      // Mouse leave event
+      map.on("mouseleave", "circles", function () {
+        // Reset the last Id
+        lastId = undefined;
+        map.getCanvas().style.cursor = "";
+        popup.remove();
+      });
+
     });
 
   }, [data]);
